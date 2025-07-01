c6c4585b236cade171ce4408de5a26d2b8c3e53c
kyosuke <kyosuke.k.94@gmail.com>
2025-07-01 13:37:17 +0900

    add docker

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,13 +3,25 @@ return {
     'akinsho/toggleterm.nvim', 
     version = "v2.13.1",
     cmd = { "ToggleTerm" },
-    opts = {
-      size = 20,
-      direction = 'horizontal',
-      hide_numbers = true,
-    },
     keys = {
       { '<Esc>', '<C-\\><C-n>', mode = 't' },
-    }
+    },
+    config = function()
+      require("toggleterm").setup({
+        direction = "float",
+        hide_numbers = true,
+      })
+
+      local Terminal = require("toggleterm.terminal").Terminal
+      local lazydocker = Terminal:new({
+        cmd = "lazydocker",
+        hidden = true,
+        direction = "float",
+      })
+
+      vim.keymap.set("n", "<leader>dd", function()
+        lazydocker:toggle()
+      end, { desc = "Toggle Lazydocker" })
+    end
   }
 }
